a059a6bd1b45d2952a42d73a931c7020e8c80f6a
benyakir <benyakir.horowitz@gmail.com>
2023-01-01 09:14:28 -0700

    add test for UserResponse::multiple

--- src/graphql/query/tests/user.rs
+++ src/graphql/query/tests/user.rs
@@ -8,7 +8,7 @@ mod user_response {
     use sea_orm::prelude::Uuid;
 
     #[test]
-    fn single_create_model() {
+    fn create_single_model() {
         let id = Uuid::new_v4();
         let model = user_entity::Model {
             id,
@@ -24,5 +24,32 @@ mod user_response {
         assert_eq!(got.name, "test user");
         assert_eq!(got.role, Role::Student);
         assert_eq!(got.status, Status::Hidden);
+    }
+
+    #[test]
+    fn create_multiple_models() {
+        let ids: Vec<Uuid> = (0..9).map(|_| Uuid::new_v4()).collect();
+        let roles = vec![Role::Guest, Role::Student, Role::Teacher, Role::Admin];
+        let statuses = vec![Status::Online, Status::Offline, Status::Hidden];
+        let models: Vec<user_entity::Model> = (0..9)
+            .map(|i| user_entity::Model {
+                id: ids[i].clone(),
+                name: format!("test user{}", i),
+                email: format!("test{}@test.com", i),
+                password: "passwordhash".to_string(),
+                role: roles[i % 4].clone(),
+                status: statuses[i % 3].clone(),
+            })
+            .collect();
+        let responses = UserResponse::multiple(models);
+
+        for i in 0..9 {
+            let got = &responses[i];
+            assert_eq!(got.id, ids[i].to_string());
+            assert_eq!(got.email, format!("test{}@test.com", i));
+            assert_eq!(got.name, format!("test user{}", i));
+            assert_eq!(got.role, roles[i % 4].clone());
+            assert_eq!(got.status, statuses[i % 3].clone());
+        }
     }
 }
