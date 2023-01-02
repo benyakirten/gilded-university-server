c2c942527128c01428602b760f265654e1571506
benyakir <benyakir.horowitz@gmail.com>
2023-01-01 18:27:20 -0700

    add hash tests

--- src/auth/hash.rs
+++ src/auth/hash.rs
@@ -17,3 +17,42 @@ pub fn verify(password: &str, hash: &str) -> Result<(), Error> {
     let parsed_hash = PasswordHash::new(hash)?;
     Pbkdf2.verify_password(password, &parsed_hash)
 }
+
+#[cfg(test)]
+mod test {
+    use pbkdf2::{
+        password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+        Pbkdf2,
+    };
+    use rand_core::OsRng;
+
+    use super::{hash, verify};
+
+    #[test]
+    fn create_password_hash() {
+        let result = hash("testpassword").unwrap();
+        let parsed_hash = PasswordHash::new(&result).unwrap();
+        let got = Pbkdf2.verify_password("testpassword".as_bytes(), &parsed_hash);
+        assert!(got.is_ok());
+    }
+
+    #[test]
+    fn verify_match_returns_ok() {
+        let password = "testpassword".as_bytes();
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = Pbkdf2.hash_password(password, &salt).unwrap().to_string();
+        let got = verify("testpassword", &password_hash);
+
+        assert!(got.is_ok());
+    }
+
+    #[test]
+    fn verify_mismatch_returns_error() {
+        let password = "testpassword".as_bytes();
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = Pbkdf2.hash_password(password, &salt).unwrap().to_string();
+        let got = verify("differentpassword", &password_hash);
+
+        assert!(got.is_err());
+    }
+}
